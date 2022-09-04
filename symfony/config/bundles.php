e5383eaccd992e28373f97c6f91a2630471821fd
Sorin Loghin <sorin.loghin@in2code.de>
2022-09-05 02:21:19 +0300

    [TASK] Update configuration

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -3,12 +3,12 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['prod' => false, 'dev' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['prod' => false, 'dev' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['prod' => false, 'dev' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
